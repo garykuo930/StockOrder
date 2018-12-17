c0f0f162b05c6d0a32681609f865ac91ef554383
ji3g4vul3eji <41719417+ji3g4vul3eji@users.noreply.github.com>
2018-12-17 17:22:29 +0800

    Done

--- StockOrder/Controllers/StockOrderController.cs
+++ StockOrder/Controllers/StockOrderController.cs
@@ -53,48 +53,151 @@ namespace StockOrderMVC.Controllers
                 SellAmount = o.Where(od => od.OrderType == "selling").Sum(od => od.Amount)
             });
 
-            Session["OrderList"] = q.ToList();
-            return PartialView(q);
+            int AccBuying = 0;
+            int AccSelling = q.Sum(o => o.SellAmount);
+
+            var GroupedOrderList = q.ToList();
+
+            foreach (var i in GroupedOrderList)
+            {
+                AccBuying += i.BuyAmount;
+
+                i.AccBuyAmount = AccBuying;
+                i.AccSellAmount = AccSelling;
+
+                AccSelling -= i.SellAmount;
+            }
+
+            Session["OrderList"] = GroupedOrderList;
+            return PartialView(GroupedOrderList);
         }
 
         public ActionResult CallAuction()
         {
             List<StockOrderVM> orderlist = Session["OrderList"] as List<StockOrderVM>;
+            List<decimal> checkpoints = new List<decimal>();
+            List<decimal> prices = new List<decimal>();
 
-            int AccBuying = 0;
-            int AccSelling = orderlist.Sum(o => o.SellAmount);
-            int index = 0;
-            decimal CheckPoint = 1;
-            decimal tempCheckPoint = 0;
+            decimal dealAmount = 0;
+            decimal resultPrice = 0;
+            int resultOrders = 0;
 
-            while (tempCheckPoint - CheckPoint < 0 )
+            foreach (var i in orderlist)
             {
-                AccBuying += orderlist[index].BuyAmount;
-
-                CheckPoint = tempCheckPoint;
-
-                tempCheckPoint = Math.Abs(AccBuying - AccSelling) * orderlist[index].TargetPrice;
-
-                if(CheckPoint == 0)
+                if (i.AccBuyAmount >= i.AccSellAmount)
                 {
-                    CheckPoint = tempCheckPoint + 1;
+                    dealAmount = i.AccSellAmount * i.TargetPrice;
+                }
+                else
+                {
+                    dealAmount = i.AccBuyAmount * i.TargetPrice;
                 }
 
-                AccSelling -= orderlist[index].SellAmount;
-                index++;
+                checkpoints.Add(dealAmount);
+                prices.Add(i.TargetPrice);
             }
 
-            ViewBag.FinalPrice = orderlist[index-1].TargetPrice;
+            for (int i = 0; i < checkpoints.Count; i++)
+            {
+                if (checkpoints[i] == checkpoints.Max())
+                {
+                    resultPrice = prices[i];
+                }
+            }
 
-            return PartialView();
+            foreach (var i in orderlist)
+            {
+                if (i.TargetPrice > resultPrice)
+                {
+                    i.BuyAmount = 0;
+                }
+                else if (i.TargetPrice < resultPrice)
+                {
+                    i.SellAmount = 0;
+                }
+                else
+                {
+                    if (i.AccBuyAmount > i.AccSellAmount)
+                    {
+                        i.BuyAmount = i.AccBuyAmount - i.AccSellAmount;
+                        i.SellAmount = 0;
+                        resultOrders = i.AccSellAmount;
+                    }
+                    else
+                    {
+                        i.BuyAmount = 0;
+                        i.SellAmount = i.AccSellAmount - i.AccBuyAmount;
+                        resultOrders = i.AccBuyAmount;
+                    }
+                }
+
+            }
+
+            ViewBag.resultPrice = resultPrice;
+            ViewBag.resultOrders = resultOrders;
+            Session["OrderList_CA"] = orderlist;
+
+            return PartialView(orderlist);
         }
 
-        public ActionResult CallByTrade()
+        public ActionResult CallByTrade(StockOrder order)
         {
-            List<StockOrderVM> orderlist = TempData["OrderList"] as List<StockOrderVM>;
+            List<StockOrderVM> orderlist = Session["OrderList_CA"] as List<StockOrderVM>;
 
+            if (order.OrderType == "buying")
+            {
+                var selling = orderlist.Where(o => o.TargetPrice <= order.TargetPrice && o.SellAmount != 0).ToList();
 
-            return PartialView();
+                if(selling.Count == 0)
+                {
+                    var target = orderlist.Where(o => o.TargetPrice == order.TargetPrice).Single();
+                    target.BuyAmount += order.Amount;
+                }
+                else
+                {
+                    for (int i = selling.Count - 1; i >= 0; i--)
+                    {
+                        if (order.Amount > selling[i].SellAmount)
+                        {
+                            order.Amount -= selling[i].SellAmount;
+                            selling[i].SellAmount = 0;
+                        }
+                        else
+                        {
+                            selling[i].SellAmount -= order.Amount;
+                            order.Amount = 0;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                var buying = orderlist.Where(o => o.TargetPrice >= order.TargetPrice && o.BuyAmount != 0).ToList();
+
+                if(buying.Count == 0)
+                {
+                    var target = orderlist.Where(o => o.TargetPrice == order.TargetPrice).Single();
+                    target.SellAmount += order.Amount;
+                }
+                else
+                {
+                    foreach (var i in buying)
+                    {
+                        if (order.Amount > i.BuyAmount)
+                        {
+                            order.Amount -= i.BuyAmount;
+                            i.BuyAmount = 0;
+                        }
+                        else
+                        {
+                            i.BuyAmount -= order.Amount;
+                            order.Amount = 0;
+                        }
+                    }
+                }
+            }
+
+            return PartialView(orderlist);
         }
     }
 }
--- StockOrder/Models/StockOrderVM.cs
+++ StockOrder/Models/StockOrderVM.cs
@@ -5,10 +5,24 @@ using System.Web;
 
 namespace StockOrderMVC.Models
 {
-    public class StockOrderVM
+    public class StockOrderVM:IComparable<StockOrderVM>
     {
+        public int AccBuyAmount { get; set; }
+        public int AccSellAmount { get; set; }
         public decimal TargetPrice { get; set; }
         public int BuyAmount { get; set; }
         public int SellAmount { get; set; }
+
+        public int CompareTo(StockOrderVM other)
+        {
+            if(other == null)
+            {
+                return 1;
+            }
+            else
+            {
+                return this.TargetPrice.CompareTo(other.TargetPrice);
+            }
+        }
     }
 }
